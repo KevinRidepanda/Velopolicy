d7a38380297820f1b006d3fe32ffcd4fc1cd69ab
KevinRidepanda <kevin@ridepanda.com>
2026-04-02 13:58:01 -0400

    Update app.js

--- velopolicy-netlify/public/js/app.js
+++ velopolicy-netlify/public/js/app.js
@@ -215,7 +215,7 @@ Return 4-6 items.`;
 
   try {
     const data = await callClaude({
-      max_tokens: 1500,
+      max_tokens: 600,
       tools: [{ type: 'web_search_20250305', name: 'web_search' }],
       messages: [{ role: 'user', content: prompt }],
     });
